3eb69e2fa9861fcde88aaf24f0733e885bcccfe1
Youngseo Kim <yk796@cornell.edu>
2023-12-07 18:19:12 +0000

    final

--- src/runsim.sh
+++ src/runsim.sh
@@ -1,33 +1,87 @@
 # #!/bin/bash
 
-# scenario b
-python SiouxFalls.py --alpha 0.15 --beta 4 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
-python SiouxFalls.py --alpha 0.15 --beta 4 --mip_gap 0.01 --n_ods 50 --transit_scenario 2 --n_bins 10
-python SiouxFalls.py --alpha 0.15 --beta 4 --mip_gap 0.01 --n_ods 100 --transit_scenario 2 --n_bins 10
-python SiouxFalls.py --alpha 0.15 --beta 4 --mip_gap 0.01 --n_ods 528 --transit_scenario 2 --n_bins 10
+# # THIRD TRIAL
+# # baseline
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
 
-# # scenario a
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 5 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 50 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 100 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 528 --transit_scenario 2 --n_bins 10
+# # number of demand
+# python SiouxFalls.py --n_ods 10 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 500 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
 
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 5 --transit_scenario 2 --n_bins 100
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 100
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 50 --transit_scenario 2 --n_bins 100
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 100 --transit_scenario 2 --n_bins 100
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 528 --transit_scenario 2 --n_bins 100
+# # number of bins
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 20 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 30 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
 
-# # scenario b
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0.15 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0.5 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
+# # congestion function
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
 
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 1 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 2 --n_bins 10
-# python SiouxFalls.py --alpha 0 --mip_gap 0.01 --n_ods 10 --transit_scenario 3 --n_bins 10
+# # transit network
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 1 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 3 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
 
-# #ideal
-# python SiouxFalls.py --alpha 0.15 --mip_gap 0.01 --n_ods 528 --transit_scenario 2 --n_bins 100
+# # operation costs
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.20 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.32 --mip_gap 0.05 --time_limit 10800
 
+# # demand distribution
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# # FOURTH TRIAL
+# # number of bins
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 50 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 100 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# # congestion function
+# python SiouxFalls.py --n_ods 500 --dist 1 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 500 --dist 1 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+
+# FIFTH TRIAL
+# number of bins
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 40 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 60 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 80 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# # congestion function
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+
+# # distribution
+# python SiouxFalls.py --n_ods 528 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 576 --dist 2 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# python SiouxFalls.py --n_ods 500 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 500 --dist 2 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# # congestion function
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.15 --beta 2 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.15 --beta 3 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.1 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 2 --alpha 0.2 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.01 --time_limit 10800
+
+# # SIXTH TRIAL
+
+# # number of bins
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 3 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 7 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 20 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 55 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 148 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 403 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# # congest function
+# python SiouxFalls.py --n_ods 528 --dist 1 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 528 --dist 1 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 528 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+# python SiouxFalls.py --n_ods 576 --dist 2 --alpha 0 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 576 --dist 2 --alpha 0.15 --beta 1 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+# python SiouxFalls.py --n_ods 576 --dist 2 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 10 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+
+python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 2981 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
+python SiouxFalls.py --n_ods 100 --dist 1 --alpha 0.15 --beta 4 --transit_scenario 2 --n_bins 8103 --oper_cost 0.08 --mip_gap 0.05 --time_limit 10800
